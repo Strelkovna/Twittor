0e63b56b9b8024ea0e591f7dce703c9c9ba910f5
DK\oltr <oltr@sitecore.net>
2018-08-31 14:39:19 +0300

    Initial set up

---
+++ Twittor/Twittor/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TweetSharp;
+using System.Timers;
+
+
+namespace Twittor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
